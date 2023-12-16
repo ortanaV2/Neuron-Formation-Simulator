4ac71f521b8a4c4fa64b1d9f0ea76b36df4cb35c
ortanaV2 <ortana.v2@outlook.com>
2023-12-16 14:30:04 +0100

    Canvasbase build

--- New Version/NeuronSim.py
+++ New Version/NeuronSim.py
@@ -1,47 +1,48 @@
 import tkinter as tk
 import random
 
-class GridApp:
-    """Grid Base"""
-    show_text = False
+class NeuronSim:
+    first_text = True
+    text = None
     def __init__(self, root):
         self.root = root
         self.root.title("NeuronGrowthSim")
 
-        self.canvas = tk.Canvas(root, width=800, height=500, borderwidth=0, highlightthickness=0)
+        self.canvas = tk.Canvas(root, width=900, height=600, borderwidth=0, highlightthickness=0, bg="#0f0f0f")
         self.canvas.pack(expand=True, anchor="center")
-
-        self.canvas.pack()
 
         self.rows = 500
         self.columns = 800
-        self.cell_width = 1
-        self.cell_height = 1
+        self.cell_width = 2
+        self.cell_height = 2
 
         self.loop_interval = 1
         self.start_loop()
 
-    def draw_gridbox(self, x, y):
+    def draw_gridbox(self, x, y, color):
         x1 = x * self.cell_width
         y1 = y * self.cell_height
         x2 = x1 + self.cell_width
         y2 = y1 + self.cell_height
-        self.canvas.create_rectangle(x1, y1, x2, y2, outline="black", fill="black")
+        rect = self.canvas.create_rectangle(x1, y1, x2, y2, outline=color, fill=color)
 
-        if not GridApp.show_text:
-            text_x = 400
-            text_y = 400
-            self.canvas.create_text(text_x, text_y, text="Text unterhalb", anchor="center")
-            GridApp.show_text = True
+        # Upper right Text  
+        if NeuronSim.first_text:
+            text_x = 840
+            text_y = 20
+            NeuronSim.text = self.canvas.create_text(text_x, text_y, text="Text unterhalb", font=("Bahnschrift", 12), fill="white")
+            NeuronSim.first_text = False
+        else:
+            self.canvas.tag_raise(NeuronSim.text, rect) 
 
     def start_loop(self):
         self.root.after(self.loop_interval, self.loop)
 
     def loop(self):
-        self.draw_gridbox(random.randint(0, 800), random.randint(0, 500))
+        self.draw_gridbox(random.randint(0, 800), random.randint(0, 500), "blue")
         self.start_loop()
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = GridApp(root)
+    app = NeuronSim(root)
     root.mainloop()
